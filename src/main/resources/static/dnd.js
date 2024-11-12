84bcdf139bef00f58be995218bddba21e9caca51
atiki <test@test.com>
2024-11-12 18:27:55 +0700

    блок статистики alpha4

--- src/main/resources/static/dnd.js
+++ src/main/resources/static/dnd.js
@@ -3,11 +3,12 @@ window.onload = function js() {
     styleSheet = document.styleSheets[1];
 }
 
-let idIndicator = 0;
 let mouseIndicator = [
     {"class": "object",  "px": 100},
     {"class": "statistic", "px": 1}
 ];
+
+let statisticIndicator = [];
 
 async function loadMap() {
     console.log("Загрузка карты...");
@@ -18,26 +19,26 @@ async function loadMap() {
     {
         let [json] = await Promise.all([{
             "tiles": [
-                {"visible": true, "walkable": false, "type": "fire", "absX": 0, "absY": 100},
-                {"visible": true, "walkable": true, "type": "stone", "absX": 50, "absY": 0},
-                {"visible": true, "walkable": true, "type": "grass", "absX": 100, "absY": 0},
-                {"visible": true, "walkable": true, "type": "fire", "absX": 0, "absY": 50},
-                {"visible": true, "walkable": true, "type": "water", "absX": 50, "absY": 50},
-                {"visible": true, "walkable": true, "type": "fire", "absX": 100, "absY": 50},
-                {"visible": true, "walkable": true, "type": "stone", "absX": 0, "absY": 100},
-                {"visible": true, "walkable": true, "type": "fire", "absX": 50, "absY": 100},
-                {"visible": true, "walkable": true, "type": "fire", "absX": 100, "absY": 100}
+                {"visible": true, "walkable": false, "type": "fire", "absX": 0, "absY": 0},
+                {"visible": true, "walkable": true, "type": "stone", "absX": 1, "absY": 0},
+                {"visible": true, "walkable": true, "type": "grass", "absX": 2, "absY": 0},
+                {"visible": true, "walkable": true, "type": "fire", "absX": 0, "absY": 1},
+                {"visible": true, "walkable": true, "type": "water", "absX": 1, "absY": 1},
+                {"visible": true, "walkable": true, "type": "fire", "absX": 2, "absY": 1},
+                {"visible": true, "walkable": true, "type": "stone", "absX": 0, "absY": 2},
+                {"visible": true, "walkable": true, "type": "fire", "absX": 1, "absY": 2},
+                {"visible": true, "walkable": true, "type": "fire", "absX": 2, "absY": 2}
             ],
             "characters": [
-                {"type": "bard", "absX": 100, "absY": 100, "level": 1, "abilities": [{"str": 10}, {"dex": 10}, {"con": 10}, {"int": 10}, {"wis": 10}, {"cha": 10}],},
-                {"type": "bard", "absX": 100, "absY": 400},
-                {"type": "barbarian", "absX": 300, "absY": 200}
+                {"type": "bard", "absX": 2, "absY": 2, "level": 1, "abilities": [{"str": 10}, {"dex": 10}, {"con": 10}, {"int": 10}, {"wis": 10}, {"cha": 10}],},
+                {"type": "bard", "absX": 2, "absY": 3},
+                {"type": "barbarian", "absX": 6, "absY": 4}
             ],
             "enemies": [
-                {"type": "goblin", "absX": 100, "absY": 400}
+                {"type": "goblin", "absX": 3, "absY": 4}
             ],
             "otherObjects": [
-                {"type": "table", "absX": 500, "absY": 400}
+                {"type": "table", "absX": 5, "absY": 4}
             ]
         }])
         for (let i = 0; i < json.tiles.length; i++) {
@@ -52,7 +53,7 @@ async function loadMap() {
                 let style = "background-image: url(pictures/tiles/" + json.tiles[i].type + ".png)";
                 styleSheet.insertRule(`${selector} { ${style} }`, styleSheet.cssRules.length);
             }
-            const str = `<div class="tile ${json.tiles[i].type}" style=" background-size: ${mouseIndicator[0].px}px; width: ${mouseIndicator[0].px}px; height: ${mouseIndicator[0].px}px; left: ${json.tiles[i].absX}px; top: ${json.tiles[i].absY}px;"></div>`
+            const str = `<div class="tile ${json.tiles[i].type}" style=" background-size: ${mouseIndicator[0].px}px; width: ${mouseIndicator[0].px}px; height: ${mouseIndicator[0].px}px; left: ${json.tiles[i].absX * mouseIndicator[0].px}px; top: ${json.tiles[i].absY * mouseIndicator[0].px}px;"></div>`
             $("#map").append(str);
         }
         console.log("Map done");
@@ -66,8 +67,10 @@ async function loadMap() {
             if (ind === 0) {
                 cssAddRule('.' + json.characters[i].type, "background-image: url(pictures/characters/" + json.characters[i].type + ".png)")
             }
-            const str1 = `<div id="c${i}" class="object ${json.characters[i].type}" style=" background-size: ${mouseIndicator[0].px}px; width: ${mouseIndicator[0].px}px; height: ${mouseIndicator[0].px}px; left: ${json.characters[i].absX}px; top: ${json.characters[i].absY}px;"></div>`
+            const str1 = `<div id="c${i}" class="object ${json.characters[i].type}" style=" background-size: ${mouseIndicator[0].px}px; width: ${mouseIndicator[0].px}px; height: ${mouseIndicator[0].px}px; left: ${json.characters[i].absX * mouseIndicator[0].px}px; top: ${json.characters[i].absY * mouseIndicator[0].px}px;"></div>`
             $("#map").append(str1);
+            let SI = `c${i}`;
+            statisticIndicator[SI] = 0;
         }
         console.log("Characters done");
         for (let i = 0; i < json.enemies.length; i++) {
@@ -82,8 +85,10 @@ async function loadMap() {
                 let style = "background-image: url(pictures/enemies/" + json.enemies[i].type + ".png)";
                 styleSheet.insertRule(`${selector} { ${style} }`, styleSheet.cssRules.length);
             }
-            const str1 = `<div id="e${i}" class="object ${json.enemies[i].type}" style=" background-size: ${mouseIndicator[0].px}px; width: ${mouseIndicator[0].px}px; height: ${mouseIndicator[0].px}px; left: ${json.enemies[i].absX}px; top: ${json.enemies[i].absY}px;"></div>`
+            const str1 = `<div id="e${i}" class="object ${json.enemies[i].type}" style=" background-size: ${mouseIndicator[0].px}px; width: ${mouseIndicator[0].px}px; height: ${mouseIndicator[0].px}px; left: ${json.enemies[i].absX * mouseIndicator[0].px}px; top: ${json.enemies[i].absY * mouseIndicator[0].px}px;"></div>`
             $("#map").append(str1);
+            let SI = `e${i}`;
+            statisticIndicator[SI] = 0;
         }
         console.log("Enemies done");
         for (let i = 0; i < json.otherObjects.length; i++) {
@@ -98,8 +103,10 @@ async function loadMap() {
                 let style = "background-image: url(pictures/otherObjects/" + json.otherObjects[i].type + ".png)";
                 styleSheet.insertRule(`${selector} { ${style} }`, styleSheet.cssRules.length);
             }
-            const str1 = `<div id="o${i}" class="object ${json.otherObjects[i].type}" style="background-size: ${mouseIndicator[0].px}px; width: ${mouseIndicator[0].px}px; height: ${mouseIndicator[0].px}px; left: ${json.otherObjects[i].absX}px; top: ${json.otherObjects[i].absY}px;"></div>`
+            const str1 = `<div id="o${i}" class="object ${json.otherObjects[i].type}" style="background-size: ${mouseIndicator[0].px}px; width: ${mouseIndicator[0].px}px; height: ${mouseIndicator[0].px}px; left: ${json.otherObjects[i].absX * mouseIndicator[0].px}px; top: ${json.otherObjects[i].absY * mouseIndicator[0].px}px;"></div>`
             $("#map").append(str1);
+            let SI = `o${i}`;
+            statisticIndicator[SI] = 0;
         }
         console.log("All objects done");
         addListeners();
@@ -136,8 +143,12 @@ function click(e){
     }
 }
 
+let statId;
+let cStatId;
+let cBId;
+
 function statistic(e) {
-    let a = e.target.id;
+    window.a = e.target.id;
     let b;
     if (a[0] === "c") {
         b = "characters";
@@ -148,17 +159,61 @@ function statistic(e) {
     if (a[0] === "o") {
         b = "otherObjects";
     }
-    const str = `<div id = "s${a}"class="statistic ${b}" style="left: ${e.clientX}px; top: ${e.clientY}px;"><p>sdjahuiahfwehgiyweghfowahefuwiahfiuwaegofyowiagfoawfgyiwafwa</p><div id="c-b${a}" class="close-button"></div></div>`;
+    const str = `<div id = "${statisticIndicator[a]}${a}"class="statistic ${b}" style="left: ${e.clientX}px; top: ${e.clientY}px;"><p>sdjahuiahfwehgiyweghfowahefuwiahfiuwaegofyowiagfoawfgyiwafwa</p><div id="c-b${statisticIndicator[a]}${a}" class="close-button" onClick="Del(this)"></div></div>`;
+    statisticIndicator[a] += 1;
+    console.log(statisticIndicator);
     $("#map").append(str);
-    $(".close-button").on( "click", function(event) {
-        let statId = event.target.id;
-        let objectId = statId.slice(3);
-        $(`#s${objectId}`).remove();
-    });
+    // $(".close-button").on( "click", function(event) {
+    //     statId = event.target.parentNode.id;
+    //     cStatId = statId.slice(0, Math.max(statId.indexOf("c"), statId.indexOf("e"),statId.indexOf("o")));
+    //     cBId = statId.slice(Math.max(statId.indexOf("c"), statId.indexOf("e"),statId.indexOf("o")));
+    //     $(`#${statId}`).remove();
+    //     // let m = document.getElementById(`#${statId}`);
+    //     // m.remove();
+    //     console.log("I delete: " + statId)
+    //
+    //     console.log(statisticIndicator);
+    //     if (Number(cStatId) < statisticIndicator[a]) {
+    //         statisticIndicator[a] -= 1;
+    //         setTimeout(sortId, 0, Number(cStatId), statisticIndicator[a], cBId);
+    //     }
+    // }
+    // );
 
     $(function() {
         $('.statistic').draggable({
             grid: [mouseIndicator[1].px,mouseIndicator[1].px]
         });
     });
+}
+
+function sortId(a, b, c) {
+    for (let i = a; i < b; i++) {
+        let d = document.getElementById(`${i+1}${c}`);
+        console.log("d.id " + d.id)
+        d.id = `${i}${c}`;
+        console.log("d.id " + d.id)
+        let db = document.getElementById(`c-b${i+1}${c}`);
+        console.log("db.id " + db.id)
+        db.id = `c-b${i}${c}`;
+    }
+    console.log("done, sort from " + a + " to " + b + " in stats of " + c);
+}
+
+function Del(el) {{
+    console.log(el)
+    statId = el.parentNode.id;
+    cStatId = statId.slice(0, Math.max(statId.indexOf("c"), statId.indexOf("e"),statId.indexOf("o")));
+    cBId = statId.slice(Math.max(statId.indexOf("c"), statId.indexOf("e"),statId.indexOf("o")));
+    $(`#${statId}`).remove();
+    console.log("I delete: " + statId)
+    console.log(statisticIndicator);
+    if (Number(cStatId) < statisticIndicator[a]) {
+        statisticIndicator[a] -= 1;
+        //setTimeout(sortId, 0, Number(cStatId), statisticIndicator[a], cBId);
+        console.log("cStatId " + cStatId + " stat " + statisticIndicator[a] + " cBId " + cBId);
+        sortId(Number(cStatId), statisticIndicator[a], cBId);
+    }
+}
+
 }
